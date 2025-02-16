a9dfb59319073753c9b14ffb431e382a1d139419
Konstantinos Mokas <kostasmokas12@gmail.com>
2025-02-17 01:03:35 +0200

    Add JSDocs

--- demo/eudi-login.js
+++ demo/eudi-login.js
@@ -1,11 +1,31 @@
 const AUTH_URL = "http://localhost";
 
+/**
+* @typedef {Object} ConfigOptions
+* @property {boolean} [AgeOver18] - Whether the user is over 18.
+* @property {boolean} [HealthID] - Whether to include Health ID attestation.
+* @property {boolean} [IBAN] - Whether to include IBAN attestation.
+* @property {boolean} [Loyalty] - Whether to include Loyalty attestation.
+* @property {boolean} [mDL] - Whether to include mobile Driver’s License (mDL) attestation.
+* @property {boolean} [MSISDN] - Whether to include MSISDN (phone number) attestation.
+* @property {boolean} [PhotoId] - Whether to include Photo ID attestation.
+* @property {boolean} [PID] - Whether to include Personal ID (PID) attestation.
+* @property {boolean} [PowerOfRepresentation] - Whether to include Power of Representation attestation.
+* @property {boolean} [PseudonymDeferred] - Whether to include Pseudonym Deferred attestation.
+* @property {boolean} [Reservation] - Whether to include Reservation attestation.
+* @property {boolean} [TaxNumber] - Whether to include Tax Number attestation.
+*/
 
+/**
+ * Handles the login process for EUDI authentication.
+ * @function EUDILogin
+ * @param {ConfigOptions} config - The configuration settings for authentication.
+ * @param {string} [target=window.location.origin] - The target URL to redirect to after authentication.
+ */
 function EUDILogin(config, target = window.location.origin) {
     window.addEventListener("message", function(event) {
         if (event.origin !== AUTH_URL) return; // Security check
         console.log("User Data:", event.data);
-        // authWindow.close();
         this.localStorage.setItem("user_data", JSON.stringify(event.data));
         if (target == window.location.origin) return;
         window.location.href = target;
@@ -34,7 +54,7 @@ function EUDILogin(config, target = window.location.origin) {
         }
     }, 500);
 
-    // Stop retrying after 5 seconds
+    // Stop retrying after 1 seconds
     setTimeout(() => clearInterval(interval), 1000);
 }
 
