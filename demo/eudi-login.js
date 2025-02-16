e90f3f9693b877d285515cc1bfcbe3452c2c4bcd
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-02-16 22:02:55 +0200

    removed config

--- demo/eudi-login.js
+++ demo/eudi-login.js
@@ -1,37 +1,6 @@
-config_t = {
-    AgeOver18: false,
-    HealthID: false,
-    IBAN: false,
-    Loyalty: false,
-    mDL: false,
-    MSISDN: false,
-    PhotoId: false,
-    PID: false,
-    PowerOfRepresentation: false,
-    PseudonymDeferred: false,
-    Reservation: false,
-    TaxNumber: false
-}
-
 const AUTH_URL = "http://localhost";
 
 function login(config, target) {
-    // const authWindow = window.open(AUTH_URL, "_blank");
-    //
-    // authWindow.onload = () => {
-    //     timeout = setTimeout(function() {
-    //         console.log("timeout")
-    //         authWindow.postMessage({
-    //             site: window.location.origin,
-    //             data: config
-    //         }, AUTH_URL);
-    //     }, 1000);
-    //     // authWindow.postMessage({
-    //     //     site: window.location.origin,
-    //     //     data: config
-    //     // }, AUTH_URL);
-    // };
-    //
     window.addEventListener("message", function(event) {
         if (event.origin !== AUTH_URL) return; // Security check
         console.log("User Data:", event.data);
